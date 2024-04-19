605f24ffac3a730758ee7bc643c34b1570063a73
Alik_Avizha <aleksandravizha@icloud.com>
2024-04-19 19:45:59 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+import { dependencies, devDependencies } from './package.json'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['es'],
+      name: 'ui-kit-by-avizha',
+    },
+    rollupOptions: {
+      external: [
+        'react/jsx-runtime',
+        ...Object.keys(dependencies),
+        ...Object.keys(devDependencies),
+      ],
+    },
+    sourcemap: true,
+    target: 'esnext',
+  },
+  plugins: [react()],
+})
